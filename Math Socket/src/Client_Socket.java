f6d4558046ecedbd50c49bb618561b8b637de9c9
seballoll <seba21541@gmail.com>
2021-09-14 20:52:31 -0600

    Sockets

--- Math Socket/src/Client_Socket.java
+++ Math Socket/src/Client_Socket.java
@@ -5,12 +5,18 @@ public class Client_Socket {
     static Socket s;
 
     public static void main(String[] args) throws IOException {
-        conect_socket();
-    }
-    public static void conect_socket() throws IOException {
         s = new Socket("localhost", 4545);
 
+        try {
+
+            while(true) {
+            }
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
+
 
 
 }
--- Math Socket/src/Server_Socket.java
+++ Math Socket/src/Server_Socket.java
@@ -15,5 +15,17 @@ public class Server_Socket {
         System.out.println("server online");//notifica al activar el socket del servidor
         s = ss.accept();
         System.out.println("client conected");//notifica cuando un cliente se conecta
+
+        try {
+
+            while(true) {
+            }
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+
     }
     }
+
