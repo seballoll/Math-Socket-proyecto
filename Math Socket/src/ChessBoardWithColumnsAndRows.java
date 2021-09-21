159da3280f6f53fcc7f39c0eeb7b7065afc51eae
okazaki2007 <86208462+okazaki2007@users.noreply.github.com>
2021-09-20 23:19:35 -0600

    Implements image

--- Math Socket/src/ChessBoardWithColumnsAndRows.java
+++ Math Socket/src/ChessBoardWithColumnsAndRows.java
@@ -1,8 +1,11 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.*;
 
@@ -13,7 +16,16 @@ public class ChessBoardWithColumnsAndRows implements ActionListener {
 
     private JButton[][] chessBoardSquares = new JButton[10][10];
     private JPanel chessBoard;
+    private final JLabel message = new JLabel(
+            "Chess Champ is ready to play!");
     public JTextField username_text = new JTextField();
+    private Image[][] chessPieceImages = new Image[2][3];
+    public static final int seen = 0, KING = 1,
+            RuOK = 2, KNIGHT = 3, BIOP = 4, PAN = 5;
+    public static final int[] STARTING_ROW = {
+             KING
+    };
+    public static final int BLACK = 0, WHITE = 1;
 
 
 
@@ -24,8 +36,20 @@ public class ChessBoardWithColumnsAndRows implements ActionListener {
 
     public final void initializeGui() {
         // set up the main GUI
+        createimage();
         gui_board.setBorder(new EmptyBorder(20, 0, 5, 5));
+        JToolBar tools = new JToolBar();
         gui_board.setLayout(new BorderLayout());
+        gui_board.add(tools, BorderLayout.PAGE_START);
+        Action newGameAction = new AbstractAction("New") {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setupNewGame();
+            }
+        };
+        tools.add(newGameAction);
+
 
 
         chessBoard = new JPanel(new GridLayout(4, 4));
@@ -35,11 +59,13 @@ public class ChessBoardWithColumnsAndRows implements ActionListener {
         Nodo current = Lista_De_Cuadros.setLista().head;
 
         // create the chess board squares
+
         Insets buttonMargin = new Insets(100,50,0,23);//manage size
         for (int ii = 0; ii < 4; ii++) {
             for (int jj = 0; jj < 4; jj++) {
                 JButton b = new JButton(current.tipo);
                 b.setMargin(buttonMargin);
+
 
 
                 b.setBackground(Color.WHITE);
@@ -62,7 +88,33 @@ public class ChessBoardWithColumnsAndRows implements ActionListener {
                 }
             }
         }
+
     }
+    private final void createimage(){
+        try{
+            URL url= new URL("https://pimmedia.egger.com/l/decor/U515_9/s/Detail/f/881x513/8803434561566");
+            BufferedImage bi= ImageIO.read(url);
+            for (int ii=0; ii<2; ii++){
+                for (int jj=0; jj<3; jj++){
+                    chessPieceImages[ii][jj]=bi.getSubimage(jj*10,ii*0,20,70);
+
+
+                }
+
+            }
+            for (int ii=0; ii<2; ii++){
+                for (int jj=0; jj<3; jj++){
+                    chessPieceImages[ii][jj]=bi.getSubimage(jj*10,ii*0,10,120);}
+
+        }
+
+        }catch(Exception e){
+            e.printStackTrace();
+            System.exit(2);
+
+    }
+        }
+
     public final void start_screen(){
 
         GridLayout layout = new GridLayout(10,10);
@@ -127,6 +179,19 @@ public class ChessBoardWithColumnsAndRows implements ActionListener {
 
     public final JComponent getGui() {
         return gui_board;
+    }
+    private final void setupNewGame() {
+        message.setText("Make your move!");
+        // set up the black pieces
+        for (int ii = 0; ii < STARTING_ROW.length; ii++) {
+            chessBoardSquares[ii][0].setIcon(new ImageIcon(
+                    chessPieceImages[BLACK][STARTING_ROW[ii]]));
+
+
+        }
+
+
+
     }
 
     public static void main(String[] args) {
