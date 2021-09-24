e540c991aa5a6f3bf05c5348525db31fa5c0934a
seballoll <seba21541@gmail.com>
2021-09-23 22:25:23 -0600

    java doc

---
+++ Math Socket/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ChessBoardWithColumnsAndRows"},{"p":"<Unnamed>","l":"Client_Socket"},{"p":"<Unnamed>","l":"dice"},{"p":"<Unnamed>","l":"Lista_Doble"},{"p":"<Unnamed>","l":"Nodo"},{"p":"<Unnamed>","l":"random_list"},{"p":"<Unnamed>","l":"Server_Socket"}];updateSearchResults();
