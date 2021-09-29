18713deec9239c5aee6b771eca7bf92a983e00c8
okazaki2007 <86208462+okazaki2007@users.noreply.github.com>
2021-09-28 20:04:23 -0600

    Delete Nodo.java

--- Math Socket/Nodo.java
+++
@@ -1,9 +0,0 @@
-import java.awt.image.BufferedImage;
-import java.io.Serializable;
-
-public class Nodo implements Serializable {
-        String tipo;
-        Nodo next;
-        Nodo prev;
-  
-}
